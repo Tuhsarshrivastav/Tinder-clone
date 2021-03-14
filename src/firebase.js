329b6e367b7d36b1275a7ab80ee648efc0cdc5aa
Tushar Shrivastav <74534919+Tuhsarshrivastav@users.noreply.github.com>
2021-03-14 08:42:49 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB_TzxYzCXiLp7q3HzGrRDVqxNDnrlF60o",
-  authDomain: "tinder-clone-35d25.firebaseapp.com",
-  projectId: "tinder-clone-35d25",
-  storageBucket: "tinder-clone-35d25.appspot.com",
-  messagingSenderId: "370141677715",
-  appId: "1:370141677715:web:9b265dfabe4d81b3b76566",
-  measurementId: "G-37CJ4DW8DC",
+//paste your firebase cdn config here
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
